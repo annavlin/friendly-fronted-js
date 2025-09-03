647977cfdb40a6b677d2b49c944b19544f8ff71d
annavlin <Anechka211299@yandex.ru>
2025-09-03 20:03:23 +0700

    Деструктуризация обьекта

--- script.js
+++ script.js
@@ -3,54 +3,16 @@
 const user = {
   name: 'Александр',
   age: 28,
-  address: {
-    city: 'Москва',
-    zipcode: 123456,
-  },
+  isDeveloper: true,
 };
 
-// console.log(user.address.city);
-console.log(user.address?.city); // с оператором optional chaning
-console.log('Какой-то текст');
+// const name = user.name;
+// const age = user.age;
+// const isDeveloper = user.isDeveloper;
 
-const guest1 = {
-  name: 'Василий',
-  age: 30,
-  orderInfo: {
-    roomType: 2,
-    stayDates: {
-      from: '14.04.2024',
-      to: '21.04.2024',
-    },
-  },
-};
+//для упрощения
+const { name, age, isDeveloper } = user;
 
-const guest2 = {
-  name: 'Екатерина',
-  age: 25,
-};
-
-// const logGuestInfo = (guest) => {
-//   const checkoutDate =
-//     guest.orderInfo && guest.orderInfo.stayDates && guest.orderInfo.stayDates.to
-//       ? guest.orderInfo.stayDates.to
-//       : null;
-
-//   console.log(`
-//         Имя: ${guest.name}
-//         Возраст: ${guest.age}
-//         Дата выезда: ${checkoutDate},
-//         `);
-// };
-
-//Более короткий вариант
-const logGuestInfo = (guest) => {
-  console.log(`
-        Имя: ${guest.name}
-       Возраст: ${guest.age}
-        Дата выезда: ${guest.orderInfo?.stayDates?.to ?? 'Не указана'},
-        `);
-};
-
-logGuestInfo(guest1);
-logGuestInfo(guest2);
+console.log('name:', name);
+console.log('age:', age);
+console.log('isDeveloper:', isDeveloper);
