07d347c903f1f89d6186e7d992f828b70414dd8e
annavlin <Anechka211299@yandex.ru>
2025-09-03 23:26:31 +0700

    this

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-'use strict';
+// 'use strict';
 
-const logUser = (user) => {
-  const { name, age, city, ...otherInfo } = user;
+// const user = {
+//   name: 'Александр',
+//   age: 28,
+//   logThis() {
+//     console.log('this в теле метода обьекта user:', this);
+//   },
+// };
 
-  console.log(`
-    Имя: ${name}
-    Возраст: ${age}
-    Город: ${city}
-    `);
-  console.log('Дополнительная информация', otherInfo);
-};
+// user.logThis();
 
-logUser({
-  name: 'Александр',
-  age: 28,
-  city: 'Москва',
-  company: 'VK',
-  jobPost: 'Фронтенд-разработчик',
-});
+const user1 = { name: 'Миша' };
+const user2 = { name: 'Вася' };
+
+function logInfo() {
+  console.log('this:', this);
+  console.log('this.name:', this.name);
+}
+logInfo();
+
+user1.logName = logInfo;
+user2.logName = logInfo;
+
+user1.logName();
+user2.logName();
