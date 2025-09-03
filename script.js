7b943c7b47c9e439dc7d5095086fe18d643d9ed8
annavlin <Anechka211299@yandex.ru>
2025-09-03 20:32:06 +0700

    Остаточные параметры

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 'use strict';
 
-const user = {
-  name: 'Александр',
+const logUser = (user) => {
+  const { name, age, city, ...otherInfo } = user;
+
+  console.log(`
+    Имя: ${name}
+    Возраст: ${age}
+    Город: ${city}
+    `);
+  console.log('Дополнительная информация', otherInfo);
 };
 
-const admin = {
-  name: 'Boss',
-};
-
-const { name: userName } = user;
-const { name: adminName } = admin;
-
-console.log('userName:', userName);
-console.log('adminName:', adminName);
-
-const user1 = {
+logUser({
   name: 'Александр',
   age: 28,
   city: 'Москва',
-};
-
-const user2 = {
-  name: 'Максим',
-  age: 5,
-};
-
-const { city = 'не указан' } = user2;
-console.log('Город:', city);
-
-const user3 = {
-  city: 'Москва',
-};
-
-const { city: userCity = 'не указано' } = user3;
-
-console.log('userCity:', userCity);
+  company: 'VK',
+  jobPost: 'Фронтенд-разработчик',
+});
