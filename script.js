39b8aabf5909f080743dcf85b5c5d868949a3dd8
annavlin <Anechka211299@yandex.ru>
2025-09-03 23:38:05 +0700

    Задача с календарем this

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-// 'use strict';
+const calculator = {
+  read() {
+    this.a = Number(prompt('Введите первое число:', 0));
+    this.b = Number(prompt('Введите второе число:', 0));
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
 
-// const user = {
-//   name: 'Александр',
-//   age: 28,
-//   logThis() {
-//     console.log('this в теле метода обьекта user:', this);
-//   },
-// };
-
-// user.logThis();
-
-const user1 = { name: 'Миша' };
-const user2 = { name: 'Вася' };
-
-function logInfo() {
-  console.log('this:', this);
-  console.log('this.name:', this.name);
-}
-logInfo();
-
-user1.logName = logInfo;
-user2.logName = logInfo;
-
-user1.logName();
-user2.logName();
+calculator.read();
+console.log('calculator', calculator);
+console.log('Сумма:', calculator.sum());
+console.log('Произведение:', calculator.mul());
