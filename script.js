36652b9a4901402b1acccce50dcde6c9feb63f28
annavlin <Anechka211299@yandex.ru>
2025-09-03 18:44:30 +0700

    Копирование обьекта

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
 'use strict';
 
-// const year = 2024;
+const obj1 = { name: 'Александр' };
+// const obj2 = Object.assign({}, obj1);
+const obj2 = { ...obj1 }; //то же самое значение
 
-// if (year === 2024) {
-//   console.log('Ура!');
-// } else if (year === 3000) {
-//   console.log('Слишком поздно. Нейросеть заменила человечество :(');
-// } else {
-//   console.log('Неизвестный год :/');
-// }
+for (const key in obj1) {
+  obj2[key] = obj1[key];
+}
 
-const obj1 = {
-  name: 'Александр',
-  age: 28,
-};
+obj2.name = 'Максим';
 
-const obj2 = {
-  name: 'Александр',
-  age: 28,
-};
-
-const areObjectsEqual = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  console.log('keys1:', keys1.length);
-  console.log('keys2:', keys2.length);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (const key in object1) {
-    const value1 = object1[key];
-    const value2 = object2[key];
-    const areValuesObjects =
-      typeof value1 === 'object' && typeof value2 === 'object';
-
-    if (areValuesObjects) {
-      return areObjectsEqual(value1, value2);
-    }
-    if (value1 !== value2) {
-      return false;
-    }
-  }
-  return true;
-};
-
-console.log('Равны ли обьекты obj1 и obj2:', areObjectsEqual(obj1, obj2));
+console.log('obj1:', obj1);
+console.log('obj2:', obj2);
