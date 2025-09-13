aa6f4fd59c3c9c55abbe48a86d5ba257e8258cad
annavlin <Anechka211299@yandex.ru>
2025-09-13 16:38:47 +0700

    JavaScript и JSON формат — парсинг и преобразование данных

--- script.js
+++ script.js
@@ -1,144 +1,41 @@
+// JSON- формат- стандартизированный формат данных в виде JS- фрагментов.
+// В качестве значений свойств JSON- обьекта могут быть строки, числа, булевые true, false-значения, null, массивы и обьекты.
+// Все строки оязательно оборачиваются в двойные кавычки
+// Висячие запятые недопустимы
+// Нужно использовать когда надо преобразовать обьект в строку
+
 const user = {
   name: 'Александр',
   age: 28,
   city: 'Москва',
+  address: {
+    street: 'ул.Пушкина, д.1, кв.1',
+    zipcode: 123456,
+  },
+  todos: ['sleep', 'eat', 'work', 'train', 'learn'],
+  hasCat: true,
+  yacht: null,
 };
 
-// Цикл FOR IN для перебора
+const userDataAsString = JSON.stringify(user); // преобразует данные JS- обьекты в JSON- обьекты в виде строки
+console.log(userDataAsString); // данные в таком виде можно отправлять на сервер
 
-for (const key in user) {
-  console.log('Имя свойства:', key);
-  console.log('Значение свойства:', user[key]);
-}
+// Если нужно наоборот преобразовать JSON- обьект в JS- обьект
 
-//Имя свойства: name
-//  Значение свойства: Александр
-//  Имя свойства: age
-//  Значение свойства: 28
-//  Имя свойства: city
-//  Значение свойства: Москва
+const parseUserData = JSON.parse(userDataAsString);
+console.log(parseUserData);
 
-// Из обьекта можем получить массив ключей
+const user1 = {
+  name: 'Александр',
+  sayHi() {
+    console.log(`Привет, ${this.name}!`);
+  },
+  car: undefined,
+};
 
-const userKeys = Object.keys(user);
-console.log('userKeys:', userKeys);
+const userDataAsString1 = JSON.stringify(user1);
+console.log(userDataAsString1); // если в преобразуемом JS- обьекте будут содержаться методы (свойства с функциями значений/ свойства со значаением underfined, то JSON.stringify не будет их учитывать, в результате будет получена только одна строка с одним свойством name )
 
-// userKeys: (3) ['name', 'age', 'city']
+const arr = ['Александр', 'Михаил', 'Василий'];
 
-userKeys.forEach((key) => {
-  console.log('Имя свойства:', key);
-});
-
-// Имя свойства: name
-//  Имя свойства: age
-//  Имя свойства: city
-
-// Получаем массив его значений
-
-const userValues = Object.values(user);
-console.log('userValues:', userValues);
-
-// userValues: (3) ['Александр', 28, 'Москва']
-
-userValues.forEach((value) => {
-  console.log('Значение свойства:', value);
-});
-
-// Значение свойства: Александр
-// Значение свойства: 28
-// Значение свойства: Москва
-
-// Получаем обьекты в виде массива
-
-const userEntries = Object.entries(user);
-console.log('userEntries', userEntries);
-
-// userEntries (3) [Array(2), Array(2), Array(2)]
-
-// Также можно перебрать используя forEach
-
-userEntries.forEach((userEntry) => {
-  const key = userEntry[0];
-  const value = userEntry[1];
-
-  console.log('Имя свойства:', key);
-  console.log('Значение свойства:', value);
-});
-
-//Имя свойства: name
-//  Значение свойства: Александр
-//  Имя свойства: age
-//  Значение свойства: 28
-//  Имя свойства: city
-//  Значение свойства: Москва
-
-// MAP
-
-const userEntries1 = Object.entries(user);
-
-const userEntriesFormatted = userEntries1.map(([key, value]) => {
-  return [key.toUpperCase(), `~~${value}~~`];
-});
-
-console.log('userEntriesFormatted:', userEntriesFormatted);
-
-// 0 : (2) ['NAME', '~~Александр~~']
-// 1 : (2) ['AGE', '~~28~~']
-// 2 : (2) ['CITY', '~~Москва~~']
-// length : 3
-
-// Если нам нужно получить из такого (в комментариях выше) обратное, нужно FROMENTRIES
-
-const userFormatted = Object.fromEntries(userEntriesFormatted);
-console.log('userFormatted:', userFormatted);
-
-// MAP
-
-//  с помощью SET добавляем элемент в коллекцию
-const data = new Map();
-data.set(1, 'Один как число'), data.set('1', 'Один как строка');
-console.log(data.get(1));
-console.log(data.get('1'));
-
-// Один как число
-// Один как строка
-
-// если хотим узнать если в коллекции элемент по определеннму ключу HAS
-
-console.log(data.has('name')); // false
-
-// но если было бы
-data.set('name', undefined); // true
-
-// Удаляет элемент DELETE точечно
-data.delete('name'); // false
-
-// Удаляет всю коллекцию целиком CLEAR
-
-data.clear();
-
-// SIZE - возвращает число/ размер
-
-console.log(data.size);
-
-// FOR OF при MAP
-
-data.set('name', 'Александр');
-data.set('age', 28);
-
-for (const key of data.keys()) {
-  console.log('key:', key);
-}
-
-for (const value of data.values()) {
-  console.log('value:', value);
-}
-
-for (const entry of data.entries()) {
-  console.log('entry:', entry);
-}
-
-// SET - коллекция, которая хранит только значения ( каждое значение может быть только в единственном экземпляре)
-
-const set = new Set([1, 2, 3, 2, 3]);
-console.log(set);
+console.log(typeof arr);
