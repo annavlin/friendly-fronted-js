ae7123cf2e186ce75ff419189233d299a67cb171
annavlin <Anechka211299@yandex.ru>
2025-09-24 23:19:50 +0700

    Браузерный JavaScript: размеры и координаты DOM-элементов

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
 const boxElement = document.querySelector('.box');
 
-// boxElement.style.position = 'absolute';
-// boxElement.style.top = '40px';
-// boxElement.style.left = '80px';
-// boxElement.style.borderWidth = '10px';
+console.log('Полная ширина:', boxElement.offsetWidth);
+console.log('Полная высота:', boxElement.offsetHeight);
 
-// полностью переписываем данные
-
-// boxElement.style.cssText = `
-// position: absolute;
-// top: 40px;
-// left: 80px;
-// width: 80px;
-// border-width: 10px
-// `;
-
-// если надо просто добавить, то пишем +=
-
-// boxElement.style.cssText += `
-// position: absolute;
-// top: 40px;
-// left: 80px;
-// width: 80px;
-// border-width: 2px
-// `;
-
-console.dir(boxElement);
-console.log(boxElement.style);
-
-// для глобального поиска в window
+console.log('Ширина левой рамки:', boxElement.clientLeft);
+console.log('Ширина верхней рамки:', boxElement.clientTop);
 
 console.log(
-  'Вычисление значения стилей boxElement:',
-  getComputedStyle(boxElement).width
+  'Расстояние от левой границы элемента до внутренностей:',
+  boxElement.clientLeft
 );
-
-// можно манипулировать с css элементами с помощью classList
-
-boxElement.classList.add('red', 'big');
-
-// чтобы удалить класс, пользоваться remove
-
-boxElement.classList.remove('red', 'big');
-
-// toggle - добавляет класс если у элемента его не было, удаляет класс у элемента если он есть
-
-boxElement.classList.toggle('red');
-
-// contains - проверяет имеет ли элемент опредеделенный класс и возвращает результат как true или false
 
 console.log(
-  'Есть ли класс "bx" у элемента boxElement?',
-  boxElement.classList.contains('bx')
+  'Расстояние от верхней границы элемента до внутренностей:',
+  boxElement.clientTop
 );
 
-// Управление CSS-переменными в JS.
+console.log('Ширина без учета рамки и скроллбара:', boxElement.clientWidth);
 
-boxElement.style.setProperty('--border-color', 'blue'); // использовать только для установки свойств цсс- переменных
+console.log('Высота без учета рамки и скроллбара:', boxElement.clientHeight);
+
+console.log(
+  'Ширина без учета рамки, включая прокручиваемую область: ',
+  boxElement.scrollWidth
+);
+
+console.log(
+  'Высота без учета рамки, включая прокручиваемую область: ',
+  boxElement.scrollHeight
+);
+
+console.log(
+  'Возможна горизонтальная прокрутка:',
+  boxElement.scrollWidth > boxElement.clientWidth
+);
+
+console.log(
+  'Возможна вертикальная прокрутка:',
+  boxElement.scrollHeight > boxElement.clientHeight
+);
+
+boxElement.scroll(10, 50); // Прокрутка содержимого элемента на 10 px вправо и 50 px вниз относительно начального положения
+
+console.log(
+  'Ширина невидимой уже прокрученной по горизонтали области: ',
+  boxElement.scrollLeft
+);
+
+console.log(
+  'Высота невидимой уже прокрученной по вертикали области: ',
+  boxElement.scrollTop
+);
+
+// получаем координаты различных элементо относительно окна браузера с помощью getBoundingClientRect()
+
+// Координаты позиции скролла страницы - window.scrollX, window.scrollY
